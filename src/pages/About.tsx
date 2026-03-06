556f2958381e5a34d9bd32a91d585cb046c6c587
mxmm1o <mxmm1oo43@gmail.com>
2026-03-07 03:06:46 +0900

    applemusic貼った

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -38,12 +38,11 @@ export function About() {
 
                         <br></br>
                         <br></br>
-                        <p>AppleMusicはる</p>
                         <iframe
                             allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
                             frameBorder="0"
                             height="450"
-                            style={{ width: "100%", maxWidth: "660px", overflow: "hidden", borderRadius: "10px" }}
+                            style={{ width: "100%", maxWidth: "400px", overflow: "hidden", borderRadius: "10px" }}
                             sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
                             src="https://embed.music.apple.com/jp/playlist/dzpls0/pl.u-RRbVY51F3x1723r"
                         />
