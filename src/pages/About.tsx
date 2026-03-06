e844cc66cb01cd1743da2fbc58d08a3445635cb2
mxmm1o <mxmm1oo43@gmail.com>
2026-03-07 03:20:13 +0900

    applemusic追加

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -41,7 +41,6 @@ export function About() {
                         <br></br>
                         <iframe
                             allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
-                            frameBorder="0"
                             height="450"
                             style={{ width: "100%", maxWidth: "500px", overflow: "hidden", borderRadius: "10px" }}
                             sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
