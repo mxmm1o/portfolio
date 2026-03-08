0f5766995419e0305a2c6c8da22911da3e6e0303
mxmm1o <mxmm1oo43@gmail.com>
2026-03-09 01:08:49 +0900

    js消した

--- src/pages/Home.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Header } from '../components/Header';
-export function Home() {
-    return (_jsxs("div", { children: [_jsx("h1", { children: "Home" }), _jsx(Header, {})] }));
-}
--- src/pages/WorkDeteil.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Header } from "../components/Header";
-export function WorkDeteil() {
-    return (_jsxs("div", { children: [_jsx("h1", { children: "\u904E\u53BB\u6848\u4EF6\u4E8B\u4F8B\u8A73\u7D30\u30DA\u30FC\u30B8" }), _jsx(Header, {})] }));
-}
