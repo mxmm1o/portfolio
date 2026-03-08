506c3a1da72d8a26542ae501a465bba742c55bcc
mxmm1o <mxmm1oo43@gmail.com>
2026-03-08 18:45:20 +0900

    Apple Music表示されないの直した

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -39,7 +39,13 @@ export function About() {
                         <br></br>
                         <br></br>
                         <br></br>
-                        <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/jp/playlist/pl.u-RRbVY51F3x1723r"></iframe>
+                        <iframe
+                            allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+                            height="450"
+                            style={{ width: "100%", maxWidth: "500px", overflow: "hidden", borderRadius: "10px" }}
+                            sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+                            src="https://embed.music.apple.com/jp/playlist/pl.u-RRbVY51F3x1723r"
+                        />
                     </div>
                 </div>
             </div >
