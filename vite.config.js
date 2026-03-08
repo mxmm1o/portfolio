2ace1cb205216f74439f55da36ae4c3bb118d04f
mxmm1o <mxmm1oo43@gmail.com>
2026-03-09 04:00:55 +0900

    パス指定

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import netlify from "@netlify/vite-plugin";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), netlify()],
-    base: './',
+    base: '/portfolio/',
 });
