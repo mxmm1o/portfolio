f7c71abcf12cbf971674e5c959003e389e0d67c6
mxmm1o <mxmm1oo43@gmail.com>
2026-03-08 18:12:08 +0900

    Homeページグリット状に並べる

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,35 @@
 import { Header } from '../components/Header'
+import styles from "./Home.module.css";
+import work1 from "../assets/loveflutter.webp"
+import work2 from "../assets/nulbarich.webp"
+import work3 from "../assets/thonght.webp"
+import work4 from "../assets/kizunaai.webp"
+import work5 from "../assets/golddisc.webp"
+import work6 from "../assets/univa.webp"
+
+
+const works = [
+  { id: 1, title: "パソコン音楽クラブ 『Love Flutter』 Release Party at Spotify O-east", image: work1 },
+  { id: 2, title: "Nulbarich SUMMER SONIC 2024 / SWEET LOVE SHOWER 2024", image: work2 },
+  { id: 3, title: "thonght nowhere Music Video", image: work3 },
+  { id: 4, title: "KizunaAI「まざる」Official Visualizer", image: work4 },
+  { id: 5, title: "GOLD DISC at ZERO TOKYO", image: work5 },
+  { id: 6, title: "UNIVERSAL STUDIO JAPAN ハロウィーンCM 「正気なんか、失え。」篇", image: work6 },
+]
+
 
 export function Home() {
   return (
-    <div><h1>Home</h1>
+    <>
       <Header />
-    </div>
-  )
-
+      <div className={styles.grid}>
+        {works.map((work) => (
+          <div key={work.id}>
+            <img src={work.image} />
+            <p>{work.title}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
