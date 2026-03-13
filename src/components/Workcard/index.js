0f2000587badf4a155f7a0594f047a267ad91956
mxmm1o <mxmm1oo43@gmail.com>
2026-03-14 01:17:57 +0900

    余計なjsができたので消した

--- src/components/Workcard/index.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import styles from "../Workcard/style.module.css";
-import { Link } from "react-router";
-export function Workcard(props) {
-    return (_jsx(Link, { to: `/works/${props.id}`, className: styles.link, children: _jsxs("div", { className: styles.card, children: [_jsx("img", { src: props.image, className: styles.img }), _jsx("div", { className: styles.overlay, children: _jsx("p", { children: props.title }) })] }) }, props.id));
-}
